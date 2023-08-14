213899f47fa57c672f49c614a729c0d1b84c21ee
Tiago Novaes <tiagousnovaes@gmail.com>
2023-08-14 12:44:52 -0300

    feat: login with github oauth

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,6 +1,7 @@
 import authenticationService, { SignInParams } from "@/services/authentication-service";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
 
@@ -8,6 +9,17 @@ export async function singInPost(req: Request, res: Response) {
     const result = await authenticationService.signIn({ email, password });
 
     return res.status(httpStatus.OK).send(result);
+  } catch (error) {
+    return res.status(httpStatus.UNAUTHORIZED).send({});
+  }
+}
+
+export async function loginGithub(req: Request, res: Response) {
+  const code = req.body.code as string;
+
+  try {
+    const token = await authenticationService.signInOauth(code);
+    return res.status(httpStatus.OK).send(token);
   } catch (error) {
     return res.status(httpStatus.UNAUTHORIZED).send({});
   }
--- src/services/authentication-service/index.ts
+++ src/services/authentication-service/index.ts
@@ -5,6 +5,72 @@ import { User } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { invalidCredentialsError } from "./errors";
+import axios from "axios";
+import qs from "qs";
+import { randomUUID } from "crypto";
+import userService from "../users-service";
+
+type GitHubParamsForAcessToken = {
+  code: string;
+  grant_type: string;
+  redirect_uri: string;
+  client_id: string;
+  client_secret: string;
+};
+
+async function exchangeCodeForAccessToken(code: string) {
+  const GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+  const { REDIRECT_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
+  const params: GitHubParamsForAcessToken = {
+    code,
+    grant_type: "authorization_code",
+    redirect_uri: REDIRECT_URL,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  };
+
+  const { data } = await axios.post(GITHUB_ACCESS_TOKEN_URL, params, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  const { access_token } = qs.parse(data);
+  return Array.isArray(access_token) ? access_token.join("") : access_token;
+}
+
+async function getEmailFromGitHub(token: string) {
+  const response = await axios.get("https://api.github.com/user/emails", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return response.data[0].email;
+}
+
+async function signInOauth(code: string) {
+  const token = await exchangeCodeForAccessToken(code) as string;
+  const email = await getEmailFromGitHub(token);
+  const existingUser = await userRepository.findByEmail(email);
+
+  let user;
+  if (existingUser) {
+    user = await getUserOrFail(email);
+  } else {
+    const randomPassword = randomUUID();
+    const newUser = await userService.createUser({ email, password: randomPassword });
+    user = await getUserOrFail(newUser.email);
+  }
+
+  const jwtToken = await createSession(user.id);
+
+  return {
+    user: exclude(user, "password"),
+    token: jwtToken
+  };
+}
+
 
 async function signIn(params: SignInParams): Promise<SignInResult> {
   const { email, password } = params;
@@ -54,6 +120,7 @@ type GetUserOrFailResult = Pick<User, "id" | "email" | "password">;
 
 const authenticationService = {
   signIn,
+  signInOauth
 };
 
 export default authenticationService;
